fdb01c882e2ca1057fccb9decfdcb3dd317a9307
Maksym-Plutenko <plutenkom@gmail.com>
2023-05-17 00:10:26 +0300

    break removed

--- index.js
+++ index.js
@@ -16,22 +16,18 @@ async function invokeAction({ action, id, name, email, phone }) {
     case "list":
       const contactList = await listContacts();
       return console.log(contactList);
-      break;
 
     case "get":
       const result = await getContactById(id);
       return console.log(result);
-      break;
 
     case "add":
       const addedContact = await addContact(name, email, phone);
       return console.log(addedContact);
-      break;
 
     case "remove":
       const removedContact = await removeContact(id);
       return console.log(removedContact);
-      break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
